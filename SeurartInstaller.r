dc4494405c1b55ab7de9f04d4e20f674052fcb15
Tony Hill <adhill1@yahoo.com>
2019-11-09 22:02:32 +0100

    Create SeurartInstaller.r

---
+++ SeurartInstaller.r
@@ -0,0 +1,18 @@
+# Install BiocManager
+if (!requireNamespace("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+
+BiocManager::install("scater")
+BiocManager::install("scran")
+BiocManager::install("DropletUtils")
+install.packages("dplyr")
+install.packages('Seurat')
+install.packages("ggplot2")
+install.packages("cowplot")
+
+
+
+
+install.packages("devtools")
+devtools::install_github(repo = "hhoeflin/hdf5r")
+devtools::install_github(repo = "mojaveazure/loomR", ref = "develop")
